2903a786d93e71c1693debc525813dbfdddb869a
umCodes <umohammed.official@gmail.com>
2025-05-19 16:03:57 +0300

    deploy commit

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const {location, setCreateInvoice, setDrafts} = useNavigate((state) => state);
   
 
-  const buttonStyle = "p-3 flex flex-col w-full m-1"
+  const buttonStyle = "p-3 flex flex-col w-full m-1 border"
   return (
     <div className="
         items-center
