13d0f08b3873f9f4caad463bf73f3ae0745b0378
umCodes <umohammed.official@gmail.com>
2025-05-19 17:41:50 +0300

    deploy commit

--- src/components/ui/List.tsx
+++ src/components/ui/List.tsx
@@ -10,9 +10,10 @@ function List() {
 
     return (
     <div 
-      className="w-full absolute bg-gray-50 h-[80dvh] bottom-0  rounded-md overflow-hidden transition-all duration-300 ease-in"
+      className="w-full absolute bg-gray-50 h-[80dvh] bottom-0  rounded-md overflow-hidden transition-all duration-200 ease-in-out"
       style={{
-        height: showList ? '700px' : '0',
+        transform: showList ? 'scaleY(1)' : 'scaleY(0)',
+        transformOrigin: 'bottom', 
         boxShadow: '-2px 3px 8px',
       }}
       >
@@ -21,7 +22,7 @@ function List() {
       <div className="flex items-center justify-between p-4 ">
       <h3 className="text-2xl text-gray-700 font-bold">Items</h3>
       <button
-        className="text-lg text-red-800"
+        className="text-lg text-blue-500"
         onClick={() => {
             setShowList(false);
         }}
