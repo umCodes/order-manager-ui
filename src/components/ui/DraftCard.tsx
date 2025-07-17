0d6c2427d5a784fa240f0497dd4cf26b14699c7d
umCodes <umohammed.official@gmail.com>
2025-07-17 22:57:01 +0300

    Update DraftCard.tsx

--- src/components/ui/DraftCard.tsx
+++ src/components/ui/DraftCard.tsx
@@ -1,56 +1,56 @@
-import type { Draft } from "../../hooks/useDrafts"
+import type { Draft } from "../../hooks/useDrafts";
 import { usePopupStore } from "../../store";
-import { addPayment } from "../../services/payments"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMoneyBillWave } from "@fortawesome/free-solid-svg-icons"
-import useDrafts from "../../hooks/useDrafts"
+import { addPayment } from "../../services/payments";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoneyBillWave } from "@fortawesome/free-solid-svg-icons";
 
+const DraftCard = ({
+  draft,
+  setLoading,
+}: {
+  draft: Draft;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const setPopup = usePopupStore((state) => state.setPopup);
 
-const DraftCard = ({draft}: {draft: Draft}) => {
-  const setPopup = usePopupStore(state => state.setPopup);
-  const {setLoading} = useDrafts();
-  
-  async function handleDraftPayment(){
-    try{
+  const handleDraftPayment = async () => {
+    try {
       await addPayment(draft);
       setPopup({
-        message: `payment recieved for ${draft.invoice_number}`,
-        success: true
-      })      
+        message: `Payment received for ${draft.invoice_number}`,
+        success: true,
+      });
       setLoading(true);
-    }catch(error){
-      console.error(error);      
+    } catch (error) {
+      console.error(error);
       setPopup({
-        message: 'An error occured making payment, please try again.',
-        success: false
-      })
+        message: "An error occurred while processing the payment.",
+        success: false,
+      });
     }
-  }
-  return ( 
-            <div className="flex items-center justify-between border-2 border-gray-200 m-3 p-2 rounded-md bg-white">
-                <div>                
-                    <h3 className="text-lg font-bold">{draft.customer_name}</h3>
-                    <p className="font-semibold">
-                        {draft.invoice_number}
-                    </p>
-                    <p className="text-gray-700 ">
-                      {draft.date}
-                    </p>
-                </div>
+  };
 
-                <div className="mx-2 ml-auto">                
-                    <span className="font-bold">
-                        SAR{draft.total}
-                    </span>
-                </div>
-              <button
-                onDoubleClick={handleDraftPayment}
-                className="cursor-pointer ml-4 p-2 text-sm text-green-700 hover:text-green-900 transition-colors"
-                title="Add Payment"
-              >
-                <FontAwesomeIcon icon={faMoneyBillWave} size="lg" />
-              </button>
-        </div>)
-}
+  return (
+    <div className="flex items-center justify-between border-2 border-gray-200 m-3 p-2 rounded-md bg-white">
+      <div>
+        <h3 className="text-lg font-bold">{draft.customer_name}</h3>
+        <p className="font-semibold">{draft.invoice_number}</p>
+        <p className="text-gray-700">{draft.date}</p>
+      </div>
 
-export default DraftCard
+      <div className="mx-2 ml-auto">
+        <span className="font-bold">SAR{draft.total}</span>
+      </div>
+
+      <button
+        onDoubleClick={handleDraftPayment}
+        className="cursor-pointer ml-4 p-2 text-sm text-green-700 hover:text-green-900 transition-colors"
+        title="Add Payment"
+      >
+        <FontAwesomeIcon icon={faMoneyBillWave} size="lg" />
+      </button>
+    </div>
+  );
+};
+
+export default DraftCard;
