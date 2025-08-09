4591ea56258e505e648f33d3bedadc3bafbc7967
umCodes <umohammed.official@gmail.com>
2025-08-09 22:15:19 +0300

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,46 +1,50 @@
-import { faClock, faReceipt } from "@fortawesome/free-solid-svg-icons"
+import { faClock, faReceipt, faClipboardList } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useNavigate } from "../../store";
 
 const Footer = () => {
-  const {location, setCreateInvoice, setDrafts} = useNavigate((state) => state);
+  const { location, setCreateInvoice, setDrafts, setRequiredItems } = useNavigate((state) => state);
   
+  const buttonStyle = "p-3 flex flex-col w-full m-1 items-center justify-center";
 
-  const buttonStyle = "p-3 flex flex-col w-full m-1 items-center justify-center"
   return (
-    <div className="
-        items-center
-        border border-gray-200 
-        bg-white 
-        pb-4
-        z-10
-        ">
-        <nav className="flex items-center">
-            <button 
-              className={buttonStyle}
-              style={{
-                color: location === 'createInvoice' ? 'blue' : ''
-              }}
-              onClick={setCreateInvoice}
-            >
-              <FontAwesomeIcon icon={faReceipt}/>
-              Create Invoices
-            </button>
-            <button               
-              className={buttonStyle}
-              style={{
-                color: location === 'drafts' ? 'blue' : ''
-              }}
-              onClick={setDrafts}
+    <div className="items-center border border-gray-200 bg-white pb-4 z-10">
+      <nav className="flex items-center">
+        <button 
+          className={buttonStyle}
+          style={{
+            color: location === 'createInvoice' ? 'blue' : ''
+          }}
+          onClick={setCreateInvoice}
+        >
+          <FontAwesomeIcon icon={faReceipt}/>
+          Create Invoices
+        </button>
 
-            >
-              <FontAwesomeIcon icon={faClock}/>
-              Drafts
+        <button               
+          className={buttonStyle}
+          style={{
+            color: location === 'drafts' ? 'blue' : ''
+          }}
+          onClick={setDrafts}
+        >
+          <FontAwesomeIcon icon={faClock}/>
+          Drafts
+        </button>
 
-            </button>
-        </nav>
+        <button               
+          className={buttonStyle}
+          style={{
+            color: location === 'requiredItems' ? 'blue' : ''
+          }}
+          onClick={setRequiredItems}
+        >
+          <FontAwesomeIcon icon={faClipboardList}/>
+          Required Items
+        </button>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
