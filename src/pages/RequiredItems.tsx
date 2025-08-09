6f94d1c085f027050d4438806a81ef51649a3a93
umCodes <umohammed.official@gmail.com>
2025-08-09 21:48:20 +0300

    Update RequiredItems.tsx

--- src/pages/RequiredItems.tsx
+++ src/pages/RequiredItems.tsx
@@ -1,6 +1,22 @@
-
+import {useState} from 'react';
 
 function RequiredItems(){
+  const [items, setitems] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  
+
+  useEffect(() => {
+    fetch('https://order-manager-api-yz7t.onrender.com/api/draftitems')
+            .then((res) => res.json())
+            .then((data) => {
+                setItems(data)        
+                setLoading(false)
+
+            })
+    }, [loading])
+
+
   return (
     <div>
       {/* Your content will go here */}
