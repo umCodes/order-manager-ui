61c9173a3b1f0a530eb36b183f4f0c9e568b1b71
umCodes <umohammed.official@gmail.com>
2025-08-09 22:21:17 +0300

    Update RequiredItems.tsx

--- src/pages/RequiredItems.tsx
+++ src/pages/RequiredItems.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate} from '../store';
 
 function RequiredItems() {
   const [items, setItems] = useState([]);
