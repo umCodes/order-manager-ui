a7ea546a4235fcebc5c553e06036fc4aac5e7bb1
umCodes <umohammed.official@gmail.com>
2026-01-18 16:52:05 +0300

    Update API URL to use VITE_API_URL variable

--- src/hooks/useCustomers.ts
+++ src/hooks/useCustomers.ts
@@ -8,7 +8,7 @@ function useCustomers() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch('https://order-manager-api-yz7t.onrender.com/api/customers')
+    fetch('`${VITE_API_URL}/api/customers`)
       .then((res) => res.json())
       .then((data) => {
         setCustomers(data)
